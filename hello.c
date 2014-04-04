123f51ee49317b98bf4b8b296305959edf2013fd
LiXianlin <xianlinli@gmail.com>
2014-04-05 04:51:21 +0800

    Update hello.c Makefile.am

--- hello.c
+++ hello.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <pthread.h>
+
+#define THREAD_NUM 5
+
+void* doJob(void* data)
+{
+    printf("Hello from thread(id=0x%lx)!\n", pthread_self());
+}
 
 int main(int argc, char **argv)
 {
-    printf("Hello!\n");
+    pthread_t threads[THREAD_NUM];
+    int i, j;
+
+    for(i = 0; i < THREAD_NUM; ++i) {
+        pthread_create(&threads[i], NULL, doJob, NULL);
+    }
+
+    for (j = 0; j < THREAD_NUM; ++j) {
+        pthread_join(threads[j], NULL);
+    }
+
     return 0;
 }
